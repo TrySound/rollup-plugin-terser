88a945a4eb6a7b6c0900d1d147bb9338962c9044
Evan Shaw <evan@vendhq.com>
2019-01-01 16:14:27 +1300

    Support Rollup 1.0.0
    Rollup 1.0.0 has been released: https://github.com/rollup/rollup/releases/tag/v1.0.0
    
    This plugin works with the 1.0.0 release from all appearances, so
    I expanded the range of versions in the peer dependency. I also updated
    the dev dependency to 1.0.0.
    
    The tests required some changes to work with 1.0.0 because the output of
    Rollup's bundle generate method has changed.

--- test/test.js
+++ test/test.js
@@ -9,10 +9,12 @@ test("minify", async () => {
     plugins: [terser()]
   });
   const result = await bundle.generate({ format: "cjs" });
-  expect(result.code).toEqual(
+  expect(result.output).toHaveLength(1);
+  const output = result.output[0];
+  expect(output.code).toEqual(
     '"use strict";window.a=5,window.a<3&&console.log(4);\n'
   );
-  expect(result.map).toBeFalsy();
+  expect(output.map).toBeFalsy();
 });
 
 test("minify via terser options", async () => {
@@ -24,8 +26,10 @@ test("minify via terser options", async () => {
     banner: "/* package name */",
     format: "cjs"
   });
-  expect(result.code).toEqual('/* package name */\n"use strict";\n');
-  expect(result.map).toBeFalsy();
+  expect(result.output).toHaveLength(1);
+  const output = result.output[0];
+  expect(output.code).toEqual('/* package name */\n"use strict";\n');
+  expect(output.map).toBeFalsy();
 });
 
 test("minify with sourcemaps", async () => {
@@ -34,7 +38,8 @@ test("minify with sourcemaps", async () => {
     plugins: [terser()]
   });
   const result = await bundle.generate({ format: "cjs", sourcemap: true });
-  expect(result.map).toBeTruthy();
+  expect(result.output).toHaveLength(1);
+  expect(result.output[0].map).toBeTruthy();
 });
 
 test("allow to disable source maps", async () => {
@@ -78,14 +83,13 @@ test("throw error on terser fail", async () => {
 test("works with code splitting", async () => {
   const bundle = await rollup({
     input: ["test/fixtures/chunk-1.js", "test/fixtures/chunk-2.js"],
-    experimentalCodeSplitting: true,
     plugins: [terser()]
   });
   const { output } = await bundle.generate({ format: "esm" });
   const newOutput = {};
-  Object.keys(output).forEach(key => {
-    const { modules, ...value } = output[key];
-    newOutput[key] = value;
+  output.forEach(out => {
+    const { modules, ...value } = out;
+    newOutput[out.fileName] = value;
   });
   expect(newOutput).toMatchSnapshot();
 });
@@ -96,7 +100,8 @@ test("allow to pass not string values to worker", async () => {
     plugins: [terser({ mangle: { properties: { regex: /^_/ } } })]
   });
   const result = await bundle.generate({ format: "cjs" });
-  expect(result.code).toEqual(
+  expect(result.output).toHaveLength(1);
+  expect(result.output[0].code).toEqual(
     '"use strict";window.a=5,window.a<3&&console.log(4);\n'
   );
 });
