63c389dbbf195817a3cc63943b57c3664a1cfb81
Luke Edwards <luke.edwards05@gmail.com>
2020-08-28 11:07:26 -0700

    fix(types): remove invalid `sourcemap` option
    Throws error if defined -- so don't expose it in definitions

--- rollup-plugin-terser.d.ts
+++ rollup-plugin-terser.d.ts
@@ -6,13 +6,6 @@ export interface Options extends Omit<MinifyOptions, "sourceMap"> {
    * Amount of workers to spawn. Defaults to the number of CPUs minus 1.
    */
   numWorkers?: number;
-
-  /**
-   * Generates source maps and passes them to rollup.
-   *
-   * @default true
-   */
-  sourcemap?: boolean;
 }
 
 export declare function terser(options?: Options): Plugin;
